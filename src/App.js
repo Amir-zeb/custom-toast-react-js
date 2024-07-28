6d6794b4e12ddd2b12ec49f918dd4d471a6e43e9
Amir-zeb <amir-zeb@iinv.tech>
2024-07-28 13:11:20 +0500

    trigger toast from form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.scss';
+import { useContext, useState } from 'react';
+import Toast from './components/Toast';
+import { ToastContext } from './context/toast-context';
 
 function App() {
+  const [toastSetting, setToastSetting] = useState({
+    type: "info",
+    message: "lorem ipsum",
+    duration: 3000,
+    position: "bottom-right",
+  });
+  const { type, duration, message } = toastSetting;
+  const { addToast } = useContext(ToastContext);
+
+  const handleInputChange = (e) => {
+    const { value, name } = e.target;
+    setToastSetting(prev => ({ ...prev, [name]: value }));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app_container'>
+      <h1>Custom Toast</h1>
+      <div className='inputs_container'>
+        <label htmlFor="">Type</label>
+        <select name="type" value={type} onChange={handleInputChange}>
+          <option value="info">Info</option>
+          <option value="error">Error</option>
+          <option value="warning">Warning</option>
+          <option value="success">Success</option>
+          <option value="message">simple message</option>
+        </select>
+        <label htmlFor="">Duration</label>
+        <input type='text' name='duration' value={duration} onChange={handleInputChange} />
+        <label htmlFor="">Message</label>
+        <textarea type='text' rows='5' name='message' value={message} onChange={handleInputChange} />
+      </div>
+      <button
+        type='button'
+        aria-label='add toast'
+        onClick={() => addToast(toastSetting)}
+      >
+        Add Toast
+      </button>
+      <Toast />
     </div>
-  );
+  )
 }
 
 export default App;
